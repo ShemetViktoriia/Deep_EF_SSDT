dacd2a87ae9c08cba6a4f10124d2a6be27efb2a7
ShemetViktoriia <shemetvic@ukr.net>
2020-01-15 01:59:36 +0200

    edmx was updated after adding the field IsActive

--- Deep_EF_SSDT.DataAccessLayer/Books.cs
+++ Deep_EF_SSDT.DataAccessLayer/Books.cs
@@ -16,5 +16,6 @@ namespace Deep_EF_SSDT.DataAccessLayer
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public Nullable<bool> IsActive { get; set; }
     }
 }
--- Deep_EF_SSDT.Database/dbo/Tables/Books.sql
+++ Deep_EF_SSDT.Database/dbo/Tables/Books.sql
@@ -2,5 +2,6 @@
 (
     [Id] INT NOT NULL,
     [Name] NVARCHAR(100) NOT NULL,
+    [IsActive] BIT NULL,
     CONSTRAINT [PK_Books] PRIMARY KEY ([Id])
 )
