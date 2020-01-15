945ab85e080827956f281b83ee76685ef034b24e
ShemetViktoriia <shemetvic@ukr.net>
2020-01-15 17:48:56 +0200

    edmx was changed after adding the function

---
+++ Deep_EF_SSDT.Database/dbo/Stored Procedures/CountOfBooksByAuthor.sql
@@ -0,0 +1,12 @@
+﻿CREATE FUNCTION [dbo].[CountOfBooksByAuthor]
+(
+    @authorId  int
+)
+RETURNS INT
+AS
+BEGIN
+    RETURN 
+    (SELECT Count(BA.BookId)
+    FROM BooksAuthors BA
+    WHERE  BA.AuthorId=@authorId)
+END
