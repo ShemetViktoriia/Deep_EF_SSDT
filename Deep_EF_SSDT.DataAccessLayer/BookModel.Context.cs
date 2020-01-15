9333ba8b75126b43f891ab442deed0bf56d96f9e
ShemetViktoriia <shemetvic@ukr.net>
2020-01-15 09:42:17 +0200

    edmx was changed after adding the table Authors

--- Deep_EF_SSDT.DataAccessLayer/BookModel.Context.cs
+++ Deep_EF_SSDT.DataAccessLayer/BookModel.Context.cs
@@ -28,5 +28,6 @@ namespace Deep_EF_SSDT.DataAccessLayer
         public virtual DbSet<BookDetails> BookDetails { get; set; }
         public virtual DbSet<Books> Books { get; set; }
         public virtual DbSet<Publishers> Publishers { get; set; }
+        public virtual DbSet<Authors> Authors { get; set; }
     }
 }
---
+++ Deep_EF_SSDT.Database/dbo/Tables/Authors.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Authors]
+(
+[Id] INT IDENTITY (1, 1) NOT NULL,
+[Name] NVARCHAR(100) NOT NULL,
+CONSTRAINT [PK_Authors] PRIMARY KEY ([Id])
+)
